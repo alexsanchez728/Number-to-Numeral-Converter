b2f4c4acb2a7410bdb1ac52536893af3171985b9
Alex Sanchez <alesanchez728@gmail.com>
2018-02-10 13:10:44 -0600

    ninty and hundred cases work

--- NumeralConverter.App/Converter.cs
+++ NumeralConverter.App/Converter.cs
@@ -14,8 +14,31 @@ namespace NumeralConverter.App
 
             while (number > 0)
             {
-
-                if (number >= 10)
+                if (number >= 100)
+                {
+                    result.RomanNumeral += "C";
+                    number -= 100;
+                    continue;
+                }
+                else if (number >= 90)
+                {
+                    result.RomanNumeral += "XC";
+                    number -= 90;
+                    continue;
+                }
+                else if (number >= 50)
+                {
+                    result.RomanNumeral += "L";
+                    number -= 50;
+                    continue;
+                }
+                else if (number >= 40)
+                {
+                    result.RomanNumeral += "XL";
+                    number -= 40;
+                    continue;
+                }
+                else if (number >= 10)
                 {
                     result.RomanNumeral += "X";
                     number -= 10;
@@ -27,22 +50,23 @@ namespace NumeralConverter.App
                     number -= 9;
                     continue;
                 }
-
                 else if (number >= 5)
                 {
                     result.RomanNumeral += "V";
                     number -= 5;
                     continue;
                 }
-
-                else if (number < 5)
+                else if (number >= 4)
                 {
-                    for (var i = 0; i <= number; i++)
-                    {
-                        result.RomanNumeral += "I";
-                        number--;
-                        continue;
-                    }
+                    result.RomanNumeral += "IV";
+                    number -= 4;
+                    continue;
+                }
+                else if (number <= 3)
+                {
+                    result.RomanNumeral += "I";
+                    number--;
+                    continue;
                 }
 
                 return result;
--- NumeralConverter.Tests/Class1.cs
+++ NumeralConverter.Tests/Class1.cs
@@ -29,54 +29,102 @@ namespace NumeralConverter.Tests
         [Test]
         public void Entering_number_five_results_in_one_V_numeral_being_printed()
         {
-            // Arrange
             var number = 5;
             var numParser = new Converter();
 
-            // Act
             var result = numParser.Numeralize(number);
 
-            // Assert
             Assert.AreEqual("V", result.RomanNumeral);
         }
         [Test]
         public void Entering_number_nine_results_in_one_I_and_one_X_numerals_being_printed()
         {
-            // Arrange
             var number = 9;
             var numParser = new Converter();
 
-            // Act
             var result = numParser.Numeralize(number);
 
-            // Assert
             Assert.AreEqual("IX", result.RomanNumeral);
         }
         [Test]
         public void Entering_number_twelve_results_in_one_X_and_two_I_numerals_being_printed()
         {
-            // Arrange
             var number = 12;
             var numParser = new Converter();
 
-            // Act
             var result = numParser.Numeralize(number);
 
-            // Assert
             Assert.AreEqual("XII", result.RomanNumeral);
         }
         [Test]
         public void Entering_number_sixteen_results_in_one_X_one_V_and_one_I_numerals_being_printed()
         {
-            // Arrange
             var number = 16;
             var numParser = new Converter();
 
-            // Act
             var result = numParser.Numeralize(number);
 
-            // Assert
             Assert.AreEqual("XVI", result.RomanNumeral);
+        }
+        [Test]
+        public void Entering_number_twentynine_results_in_two_X_one_IX_numerals_being_printed()
+        {
+            var number = 29;
+            var numParser = new Converter();
+
+            var result = numParser.Numeralize(number);
+
+            Assert.AreEqual("XXIX", result.RomanNumeral);
+        }
+        [Test]
+        public void Entering_number_fourty_four_results_in_one_X_one_L_one_I_and_one_V_numerals_being_printed()
+        {
+            var number = 44;
+            var numParser = new Converter();
+
+            var result = numParser.Numeralize(number);
+
+            Assert.AreEqual("XLIV", result.RomanNumeral);
+        }
+        [Test]
+        public void Entering_number_fourty_five_results_in_one_X_one_L_one_V_numerals_being_printed()
+        {
+            var number = 45;
+            var numParser = new Converter();
+
+            var result = numParser.Numeralize(number);
+
+            Assert.AreEqual("XLV", result.RomanNumeral);
+        }
+        [Test]
+        public void Entering_number_sixty_eight_results_in_one_L_one_X_one_V_and_three_I_numerals_being_printed()
+        {
+            var number = 68;
+            var numParser = new Converter();
+
+            var result = numParser.Numeralize(number);
+
+            Assert.AreEqual("LXVIII", result.RomanNumeral);
+        }
+        [Test]
+        public void Entering_number_eighty_three_results_in_one_L_one_X_three_V_and_three_I_numerals_being_printed()
+        {
+            var number = 83;
+            var numParser = new Converter();
+
+            var result = numParser.Numeralize(number);
+
+            Assert.AreEqual("LXXXIII", result.RomanNumeral);
+        }
+        [Test]
+        public void Entering_number_ninty_seven_results_in_one_X_one_C_one_V_and_two_I_numerals_being_printed()
+        {
+            var number = 97;
+            var numParser = new Converter();
+
+            var result = numParser.Numeralize(number);
+
+            Assert.AreEqual("XCVII", result.RomanNumeral);
         }
     }
 }
