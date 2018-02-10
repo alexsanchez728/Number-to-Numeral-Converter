aa9d09472a0efecb30f9717bdcf9038363ef0825
Alex Sanchez <alesanchez728@gmail.com>
2018-02-10 11:43:04 -0600

    numeral I passes

---
+++ NumeralConverter.App/Converter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumeralConverter.App
+{
+    public class Converter
+    {
+        public Numeral Parse(int number)
+        {
+            var converted = new Numeral
+            {
+                RomanNumeral = "I"
+            };
+
+            return converted;
+        }
+    }
+    public class Numeral
+    {
+        public string RomanNumeral { get; set; }
+    }
+}
---
+++ NumeralConverter.Tests/Class1.cs
@@ -0,0 +1,23 @@
+﻿using NumeralConverter.App;
+using NUnit.Framework;
+
+namespace NumeralConverter.Tests
+{
+    [TestFixture]
+    public class NumberConverterTests
+    {
+        [Test]
+        public void Entering_number_from_one_to_four()
+        {
+            // Arrange
+            var number = 1;
+            var numParser = new Converter();
+
+            // Act
+            var result = numParser.Parse(number);
+
+            // Assert
+            Assert.AreEqual("I", result.RomanNumeral);
+        }
+    }
+}
